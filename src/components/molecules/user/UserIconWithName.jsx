35eeeb23bf68106f0168abf176a48a8dc62ddd97
Yuma Ohno <100353174+Yono1112@users.noreply.github.com>
2023-10-03 06:15:34 +0000

    create userCard organism component

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -4,8 +4,8 @@ export const UserIconWithName = (props) => {
 	const { image, name } = props;
 	return (
 		<SContainer>
-			<img height={160} width={160} src={image} alt={name} />
-			<p>{name}</p>
+			<SImg height={160} width={160} src={image} alt={name} />
+			<SName>{name}</SName>
 		</SContainer>
 	);
 };
@@ -14,4 +14,11 @@ const SContainer = styled.div`
 	text-align: center;
 `;
 const SImg = styled.img`
-`
+	border-radius: 50%;
+`;
+const SName = styled.p`
+	font-size: 18px;
+	font-weight: bold;
+	margin: 0;
+	color: #40514e;
+`;
